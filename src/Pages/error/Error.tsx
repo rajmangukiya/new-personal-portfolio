e6afd8e4c9a96ee169dad410251b2f2312325aad
rajmangukiya <rajmangukiya00@gmail.com>
2021-09-19 18:46:28 +0530

    error page added & deploy text updated

--- src/Pages/error/Error.tsx
+++ src/Pages/error/Error.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div>
-      
+    <div className="min-vh-100">
+      <div className="container h-100 mt-5 pt-5">
+        <h1 className="display-1 fw-bold text-center">
+          Opps! Bad URL!
+        </h1>
+      </div>
     </div>
   )
 }
