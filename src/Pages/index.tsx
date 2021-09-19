c22b4484a819775ccfdec0c5bd0fda6d9e357144
rajmangukiya <rajmangukiya00@gmail.com>
2021-09-19 19:08:59 +0530

    updated

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -12,14 +12,14 @@ import Project from './portfolio/Project'
 const Index: React.FC = () => {
     return (
             <Switch>
-                <Route exact path="new-personal-portfolio/" component={Home}/>
-                <Route exact path="new-personal-portfolio/about" component={About}/>
-                <Route exact path="new-personal-portfolio/portfolio" component={Portfolio}/>
-                <Route exact path="new-personal-portfolio/portfolio/:id" component={OpenProject}/>
-                <Route exact path="new-personal-portfolio/blog" component={Blog}/>
-                <Route exact path="new-personal-portfolio/contact" component={Contact}/>
-                <Route path="new-personal-portfolio/error" component={Error} /> 
-                <Redirect from="**" to="new-personal-portfolio/error" />
+                <Route exact path="/new-personal-portfolio" component={Home}/>
+                <Route exact path="/new-personal-portfolio/about" component={About}/>
+                <Route exact path="/new-personal-portfolio/portfolio" component={Portfolio}/>
+                <Route exact path="/new-personal-portfolio/portfolio/:id" component={OpenProject}/>
+                <Route exact path="/new-personal-portfolio/blog" component={Blog}/>
+                <Route exact path="/new-personal-portfolio/contact" component={Contact}/>
+                <Route path="/new-personal-portfolio/error" component={Error} /> 
+                <Redirect from="**" to="/new-personal-portfolio/error" />
             </Switch>
     )
 }
